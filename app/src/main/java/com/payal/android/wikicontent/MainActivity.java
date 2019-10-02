bdf1f5a561e818b3c7c14f536914e453d787f366
AndroidPayal <payalagrawal2311@gmail.com>
2019-10-02 10:30:17 -0500

    commit implemented day night mode in app

--- app/src/main/java/com/payal/android/wikicontent/MainActivity.java
+++ app/src/main/java/com/payal/android/wikicontent/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,16 +18,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         setContentView(R.layout.activity_main);
 
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         dl = (DrawerLayout)findViewById(R.id.activity_main);
         toggle = new ActionBarDrawerToggle(this, dl,toolbar,R.string.Open, R.string.Close);
-
-
         dl.addDrawerListener(toggle);
         toggle.syncState();
 
