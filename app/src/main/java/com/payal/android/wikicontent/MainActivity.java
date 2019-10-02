ed4e35037676da85262ffa757b3832065409f665
AndroidPayal <payalagrawal2311@gmail.com>
2019-10-01 20:12:50 -0500

    commit with base navigation drawer addition

--- app/src/main/java/com/payal/android/wikicontent/MainActivity.java
+++ app/src/main/java/com/payal/android/wikicontent/MainActivity.java
@@ -1,14 +1,36 @@
 package com.payal.android.wikicontent;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView nv;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is comment
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        toggle = new ActionBarDrawerToggle(this, dl,toolbar,R.string.Open, R.string.Close);
+
+
+        dl.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
     }
 }
